37b3448bf152bbca40ce8ef38a413bbd8571cfdf
Luis Talavera <luis@yodopay.com>
2018-10-29 22:21:13 -0500

    Initial commit

---
+++ binary.cpp
@@ -0,0 +1,35 @@
+#ifndef BOOLEAN_H
+#define BOOLEAN_H
+
+class boolean {
+    private:
+        char* array;
+        int size;
+    
+    public:
+        boolean(int size) {
+            // TODO
+        }
+        
+        void bitOn(int position) {
+            // TODO
+        }
+             
+        void bitOff(int position) {
+            // TODO
+        }
+             
+        bool operator[](int position) {
+            // TODO
+        }
+    
+        void clear() {    
+            // TODO
+        }
+             
+        ~boolean() {
+            // TODO
+        }
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "===========================================================" << endl;
+    cout << "\Binary Array Practice" << endl;
+    cout << "===========================================================" << endl << endl;
+
+    system("read");
+    return EXIT_SUCCESS;
+}
+
